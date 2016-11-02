e45fd57616f841759bd2e5b3a26bc478ed1ce83c
jkrouse13 <jon.krouse@gmail.com>
2016-11-01 21:57:08 -0400

    changed a commit to work with heroku :(

--- db/migrate/20161101174008_create_taggings.rb
+++ db/migrate/20161101174008_create_taggings.rb
@@ -1,6 +1,5 @@
 class CreateTaggings < ActiveRecord::Migration[5.0]
   def change
-    drop_table :taggings
     create_table :taggings do |t|
       t.references :tag, foreign_key: true
       t.references :post, foreign_key: true
